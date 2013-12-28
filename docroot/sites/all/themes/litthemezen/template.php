46b314dd1829311977de8a69e3bdc5ed5ce4c502
victorkane <victorkane@gmail.com>
2013-12-28 15:52:05 +0000

    Created page template for sections content type and modified about context to create theme template variables for section title / sub-title as well as body classes for section pages for #8

--- docroot/sites/all/themes/litthemezen/template.php
+++ docroot/sites/all/themes/litthemezen/template.php
@@ -53,10 +53,13 @@ function litthemezen_preprocess_html(&$variables, $hook) {
  *   The name of the template being rendered ("page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function litthemezen_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
-}
 // */
+function litthemezen_preprocess_page(&$variables, $hook) {
+  //$variables['sample_variable'] = t('Lorem ipsum.');
+	if (isset ($variables['node'])) {
+	  $variables['theme_hook_suggestions'][] = 'page__'. $variables['node']->type;
+	}
+}
 
 /**
  * Override or insert variables into the node templates.
