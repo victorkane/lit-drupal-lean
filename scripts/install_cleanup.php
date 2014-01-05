26c49d37f0307867c3254997d05677385e0e2a25
victorkane <victorkane@gmail.com>
2014-01-05 00:58:57 +0000

    Initial drush scripting for #31

---
+++ scripts/install_cleanup.php
@@ -0,0 +1,27 @@
+<?php 
+
+//$n = node_load(1);
+//print $n->title . '\n';
+
+// Get difficult login link into user menu
+$link = array(
+		'link_title' => 'login',
+		'link_path' => 'user/login',
+		'menu_name' => 'user-menu',
+		'weight' => 0,
+		'expanded' => 0,
+);
+
+$item = array(
+		'link_path' => $link['link_path'],
+		'link_title' => $link['link_title'],
+		'menu_name' => $link['menu_name'],
+		'weight' => $link['weight'],
+		'expanded' => $link['expanded'],
+);
+
+$exists = db_query("SELECT mlid from {menu_links} WHERE link_title=:link_title AND link_path=:link_path", array(':link_title' =>  $link['link_title'], ':link_path' => $link['link_path']))->fetchField();
+// Save the record if the data does not exist
+if(!$exists) {
+	menu_link_save($item);
+}
